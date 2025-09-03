ec856f5cee933115f74ab434647ac7b3307827a0
Toonmodder <181903809+TheDownsizer@users.noreply.github.com>
2025-09-03 05:09:26 -0400

    Snooze Square Progress!

--- toontown/dna/DNAColor.py
+++ toontown/dna/DNAColor.py
@@ -8,7 +8,7 @@ class DNAColor(DNAPropertyElement):
     def __init__(self, r="1", g="1", b="1", a="1"):
         DNAPropertyElement.__init__(self)
 
-        self.color = (float(r) * 1.25, float(g) * 1.25, float(b) * 1.25, float(a))
+        self.color = (float(r) * 1.3, float(g) * 1.3, float(b) * 1.3, float(a))
 
     def _apply(self, parent):
         parent.setColorScale(self.color)
